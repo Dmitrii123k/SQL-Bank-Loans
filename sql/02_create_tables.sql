24a04303bf05e018391d83815c9537cfd1aa3202
Dmitrii123k <32319531+Dmitrii123k@users.noreply.github.com>
2026-08-09 16:22:57 +0300

    создание таблицы loans

--- sql/02_create_tables.sql
+++ sql/02_create_tables.sql
@@ -1,22 +1,24 @@
-DROP TABLE IF EXISTS loans;
-CREATE TABLE loans (
+DROP TABLE IF EXISTS loans_raw;
+
+CREATE TABLE loans_raw (
     loan_id TEXT,
     customer_id TEXT,
     loan_status TEXT,
-    current_loan_amount NUMERIC,
+    current_loan_amount TEXT,
     term TEXT,
-    credit_score NUMERIC,
-    annual_income NUMERIC,
+    credit_score TEXT,
+    annual_income TEXT,
     years_in_job TEXT,
     home_ownership TEXT,
     purpose TEXT,
-    monthly_debt NUMERIC,
-    years_credit_history NUMERIC,
-    months_since_last_delinquent NUMERIC,
-    number_open_accounts NUMERIC,
-    number_credit_problems NUMERIC,
-    current_credit_balance NUMERIC,
-    max_open_credit NUMERIC,
-    bankruptcies NUMERIC,
-    tax_liens NUMERIC
+    monthly_debt TEXT,
+    years_credit_history TEXT,
+    months_since_last_delinquent TEXT,
+    number_open_accounts TEXT,
+    number_credit_problems TEXT,
+    current_credit_balance TEXT,
+    max_open_credit TEXT,
+    bankruptcies TEXT,
+    tax_liens TEXT
 );
+
